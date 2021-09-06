e29287d6ece9ead90440e93c319abd64fb680f3f
Simao Gomes Viana <simao.gomes@toowoxx.de>
2021-09-06 10:49:55 +0200

    Allow customizing documentation link

--- confidentiality_index.go
+++ confidentiality_index.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/toowoxx/caddy2-html-injection-plugin"
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	"github.com/pkg/errors"
+	"github.com/toowoxx/caddy2-html-injection-plugin"
 	"go.uber.org/zap"
 
 	"github.com/toowoxx/caddy2-confidentiality-index-plugin/embedded"
@@ -18,15 +18,19 @@ import (
 func init() {
 	caddy.RegisterModule(Middleware{})
 	httpcaddyfile.RegisterHandlerDirective("confidentiality", parseCaddyfile)
+	httpcaddyfile.RegisterGlobalOption("confidentiality_doc_link", unmarshalDocLink)
 }
 
 type Middleware struct {
 	ConfidentialityStr string `json:"confidentiality"`
 	Confidentiality Confidentiality
+
 	injectedWriter *injection.InjectedWriter
 	injectionMiddleware *injection.Middleware
 	logger *zap.Logger
 }
+
+var documentationLink string
 
 func (Middleware) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
@@ -38,7 +42,9 @@ func (Middleware) CaddyModule() caddy.ModuleInfo {
 func (m *Middleware) Provision(ctx caddy.Context) error {
 	m.logger = ctx.Logger(m)
 	m.logger.Info("Provisioning confidentiality index plugin",
-		zap.String("ConfidentialityStr", m.ConfidentialityStr))
+		zap.String("ConfidentialityStr", m.ConfidentialityStr),
+		zap.String("documentationLink", documentationLink),
+	)
 	m.injectionMiddleware = &injection.Middleware{
 		ContentType: "text/html",
 		// Not necessary as we will be using a custom LineHandler
@@ -62,6 +68,7 @@ func (m Middleware) HandleLine(line string) (string, error) {
 	if strings.Contains(line, m.injectedWriter.M.Before) {
 		textToInject := embedded.ConfidentialityIndexHtml
 		textToInject = strings.Replace(textToInject, "{{confidentiality}}", m.Confidentiality.ToScriptKey(), 1)
+		textToInject = strings.Replace(textToInject, "{{documentation_link}}", documentationLink, 1)
 		textToInject = m.injectedWriter.HandleCSPForText(textToInject)
 		return strings.Replace(line, m.injectedWriter.M.Before, textToInject+m.injectedWriter.M.Before, 1), nil
 	}
@@ -87,7 +94,7 @@ func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 	return nil
 }
 
-func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+func (m *Middleware) unmarshalConfidentialityDir(d *caddyfile.Dispenser) error {
 	if !d.Next() {
 		return d.Err("expected token following confidentiality")
 	}
@@ -102,9 +109,24 @@ func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	return nil
 }
 
+func unmarshalDocLink(d *caddyfile.Dispenser, _ interface{}) (interface{}, error) {
+	if !d.Next() {
+		return nil, d.Err("expected token following confidentiality_doc_link")
+	}
+	if !d.NextArg() {
+		return nil, d.Err("expected argument for confidentiality_doc_link")
+	}
+	value := d.Val()
+	if len(value) == 0 {
+		return nil, d.Err("expected string after 'confidentiality_doc_link' token")
+	}
+	documentationLink = value
+	return value, nil
+}
+
 func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
 	var m Middleware
-	err := m.UnmarshalCaddyfile(h.Dispenser)
+	err := m.unmarshalConfidentialityDir(h.Dispenser)
 	return m, err
 }
 
@@ -113,6 +135,5 @@ var (
 	_ caddy.Provisioner           = (*Middleware)(nil)
 	_ caddy.Validator             = (*Middleware)(nil)
 	_ caddyhttp.MiddlewareHandler = (*Middleware)(nil)
-	_ caddyfile.Unmarshaler       = (*Middleware)(nil)
 	_ injection.LineHandler		  = (*Middleware)(nil)
 )
